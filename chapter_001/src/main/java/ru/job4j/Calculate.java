bbd22f30b653ef2ab54c7ed4d360cf0f52803392
AntonGavr92 <Sith_1@mail.ru>
2017-07-03 19:02:58 +0500

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
-public class Calculate{
-	public static void main(String[] args){
+/**
+ * Class Класс для вывода строки "Hello, world!!!" в консоли  
+ * @author agavrikov
+ * @since 03.07.2017 
+ * @version 1
+*/
+public class Calculate {
+	
+	/**
+	 * Статический метод, вызываемый при запуске программы(не уверен что здесь нужен javadoc но тем не менее)
+	 * @param args - массив с данными указанными при запуске программы
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello, world!!!");
 	}
 }
