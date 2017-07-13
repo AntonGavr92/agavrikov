58df133cf123c1f889de8589b8f57d47d6861d39
AntonGavr92 <Sith_1@mail.ru>
2017-07-13 17:25:00 +0500

    3. Конвертация двумерного массива в ArrayList и наоборот [#10035]

---
+++ chapter_003/src/main/java/ru/job4j/ConvertList.java
@@ -0,0 +1,55 @@
+package ru.job4j;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Класс для конвертации массива в ArrayList и наоборот.
+ * @author agavrikov
+ * @since 13.07.2017
+ * @version 1
+ */
+public class ConvertList {
+
+    /**
+     * Метод для конвертирования массива в ArrayList.
+     * @param array массив, который нужно конвертировать
+     * @return преобразованный массив в ArrayList
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                result.add(array[i][j]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод для конвертации ArrayList в массив.
+     * @param list ArrayList, который необходимо преобразовать
+     * @param rows количество строк в массиве
+     * @return ArrayList
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+        int length = list.size() / rows;
+        if (list.size() % rows != 0) {
+            length++;
+        }
+        Iterator<Integer> iter = list.iterator();
+        int[][] result = new int[rows][length];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < length; j++) {
+                if(iter.hasNext()) {
+                    result[i][j] = iter.next();
+                } else {
+                    result[i][j] = 0;
+                }
+
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/ConvertListTest.java
@@ -0,0 +1,92 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class ConvertList.
+ * @author Anton Gavrikov
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConvertListTest {
+    /**
+     * Тестирование метода для конвертации массива в ArrayList.
+     */
+    @Test
+    public void whenConvertList9ElementsThenListSize9Elements() {
+        ConvertList convertList = new ConvertList();
+        int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> resultList = convertList.toList(array);
+        int result = resultList.size();
+        int expected = 9;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование метода для конвертации массива в ArrayList.
+     */
+    @Test
+    public void whenConvertListAndLastElement9ThenListLastElement9() {
+        ConvertList convertList = new ConvertList();
+        int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        List<Integer> resultList = convertList.toList(array);
+        int result = resultList.get(resultList.size() - 1);
+        int expected = 9;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование метода для конвертации ArrayList в массив.
+     */
+    @Test
+    public void whenListSize9AndConvertListToArrayThenCountElementsResultArray9() {
+        ConvertList convertList = new ConvertList();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            list.add(i);
+        }
+        int[][] resultArray = convertList.toArray(list, 3);
+        int result = resultArray.length * resultArray[0].length;
+        int expected = list.size();
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование метода для конвертации ArrayList в массив.
+     */
+    @Test
+    public void whenListSize10AndConvertListToArrayThenCountElementsResultArray12() {
+        ConvertList convertList = new ConvertList();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        int[][] resultArray = convertList.toArray(list, 3);
+        int result = resultArray.length * resultArray[0].length;
+        int expected = 12;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тестирование метода для конвертации ArrayList в массив.
+     */
+    @Test
+    public void whenListSize10AndConvertListToArrayThenLastElementIs0() {
+        ConvertList convertList = new ConvertList();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        int[][] resultArray = convertList.toArray(list, 3);
+        int result = resultArray[resultArray.length - 1][resultArray[0].length - 1];
+        int expected = 0;
+        assertThat(result, is(expected));
+    }
+
+}
