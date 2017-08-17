a7bcbbb93566bfbea4b73d030ed8e5e4200a941c
AntonGavr92 <Sith_1@mail.ru>
2017-08-17 15:08:34 +0500

    5. Создать программу консольный чат.  [#862]

---
+++ chapter_003_io/src/main/java/ru/job4j/io/Chat.java
@@ -0,0 +1,55 @@
+package ru.job4j.io;
+
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * Класс, имитирующий работу чата со спец командами и логированием в рамках работы программы.
+ * @author agavrikov
+ * @since 17.08.2017
+ * @version 1
+ */
+public class Chat {
+
+    /**
+     * Точка входа в программу.
+     * @param args входящие параметры
+     */
+    public static void main(String[] args) {
+        LinkedList<String> phrases = new LinkedList<String>();
+        ClassLoader classLoader = Chat.class.getClassLoader();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader brPhrases = new BufferedReader(new InputStreamReader(classLoader.getResourceAsStream("phrases.txt")));
+             FileOutputStream fos = new FileOutputStream(classLoader.getResource("logs.txt").getFile())) {
+            String phrase = brPhrases.readLine();
+            while (phrase != null) {
+                phrases.add(phrase);
+                phrase = brPhrases.readLine();
+            }
+            Random rand = new Random();
+            boolean stoped = false;
+            String text = br.readLine();
+            while (!text.equals("закончить")) {
+                if (text.equals("стоп")) {
+                    fos.write(String.format("%s пользователь ввел стоп.%s", System.currentTimeMillis(), System.lineSeparator()).getBytes());
+                    stoped = true;
+                } else if (stoped && text.equals("продолжить")) {
+                    fos.write(String.format("%s пользователь ввел продолжить.%s", System.currentTimeMillis(), System.lineSeparator()).getBytes());
+                    stoped = false;
+                }
+                if (!stoped) {
+                    System.out.println(phrases.get(rand.nextInt(phrases.size())));
+                }
+                text = br.readLine();
+            }
+            fos.write(String.format("%s пользователь закончил работу.%s", System.currentTimeMillis(), System.lineSeparator()).getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
