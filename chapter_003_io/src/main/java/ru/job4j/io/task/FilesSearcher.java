be38fddf847ae5d01c645bf2e2e313dd7a00e86d
AntonGavr92 <Sith_1@mail.ru>
2017-08-21 15:38:39 +0500

    Ввод вывод правки.

---
+++ chapter_003_io/src/main/java/ru/job4j/io/task/FilesSearcher.java
@@ -0,0 +1,175 @@
+package ru.job4j.io.task;
+
+import java.io.*;
+
+/**
+ * Класс, реализующий поиск файлов по указаннному имени или маске в указанной директории.
+ * @author agavrikov
+ * @since 21.08.2017
+ * @version 1
+ */
+public class FilesSearcher {
+    /**
+     * Директория для поиска файлов.
+     */
+    private final String path;
+
+    /**
+     * Путь к файлу с логами, при необходимости логгирования.
+     */
+    private final String resultPath;
+
+    /**
+     * Искомое значение.
+     */
+    private final String searchVal;
+
+    /**
+     * Символ, идентифицирующий поиск осуществляемый по маске.
+     */
+    private static final String TYPE_SEARCH_BY_MASK = "-m";
+
+    /**
+     * Символ маски.
+     */
+    private static final String MASK_SYMBOl = "*";
+
+    /**
+     * Символ, идентифицирующий поиск осуществляемый по полному наименованию.
+     */
+    private static final String TYPE_SEARCH_BY_FULL_NAME = "-f";
+
+    /**
+     * Конструктор с указанным путем к логам, для инициализации полей.
+     * @param path путь
+     * @param searchVal искомое значение
+     * @param resultPath путь к файлу с логами
+     */
+    public FilesSearcher(String path, String searchVal, String resultPath) {
+        this.path = path;
+        this.resultPath = resultPath;
+        this.searchVal = searchVal;
+    }
+
+    /**
+     * Конструктор для инициализации полей.
+     * @param path путь
+     * @param searchVal искомое значение
+     */
+    public FilesSearcher(String path, String searchVal) {
+        this.path = path;
+        this.resultPath = null;
+        this.searchVal = searchVal;
+    }
+
+    /**
+     * Точка входа в программу.
+     * @param args параметры
+     */
+    public static void main(String[] args) {
+        String searchType = "-f";
+        String path = "";
+        String searchVal = "";
+        String logPath = "";
+        for (int i = 0; i < args.length; i++) {
+            if(args[i].equals("-d")) {
+                path = args[i + 1];
+            } else if (args[i].equals("-n")) {
+                searchVal = args[i + 1];
+            } else if (args[i].equals("-o")) {
+                logPath = args[i + 1];
+            } else if (args[i].equals("-m") || args[i].equals("-f")) {
+                searchType = args[i];
+            }
+        }
+        if (!path.equals("") && !searchVal.equals("")) {
+            FilesSearcher filesSearcher;
+            if (!logPath.equals("")) {
+                filesSearcher = new FilesSearcher(path, searchVal, logPath);
+            } else {
+                filesSearcher = new FilesSearcher(path, searchVal);
+            }
+            filesSearcher.searchFiles(new File(filesSearcher.path), searchType);
+        } else {
+            System.out.println("Path and search value must be not empty!");
+        }
+    }
+
+    /**
+     * Метод для проверки имени по маске.
+     * @param fileName имя файла
+     * @return если имя файла соответствует маске - true иначе false
+     */
+    private boolean checkByMask(String fileName) {
+        boolean result = false;
+        if (this.searchVal.equals(MASK_SYMBOl)) {
+            result = true;
+        } else if (fileName.contains(this.searchVal.replace(MASK_SYMBOl, ""))) {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Метод для проверки имени.
+     * @param fileName имя файла
+     * @return если имя файла идеентично искомому - true иначе false
+     */
+    private boolean checkByFullName(String fileName) {
+        boolean result = false;
+        if (this.searchVal.equals(fileName)) {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Метод, для определения необходимой функции проверки и вызывающий функцию записи лога.
+     * @param file файл
+     * @param searchType тип поиска
+     */
+    private void checkFileName(File file, String searchType) {
+        boolean accetableFile = false;
+        if(searchType.equals(TYPE_SEARCH_BY_FULL_NAME)) {
+            accetableFile = this.checkByFullName(file.getName());
+        } else if (searchType.equals(TYPE_SEARCH_BY_MASK)){
+            accetableFile = this.checkByMask(file.getName());
+        }
+        if (accetableFile && resultPath != null) {
+            createLog(file.getPath());
+        }
+    }
+
+    /**
+     * Метод для формирования файла с логами.
+     * @param path путь к файлу с логами.
+     */
+    private void createLog(String path) {
+        try (FileWriter fos = new FileWriter(resultPath, true)) {
+            fos.append(String.format("%s%s", path, System.lineSeparator()));
+            fos.flush();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    /**
+     * Рекурсивный метод, для обхода всех файлов в file.
+     * @param file директория или файл для поиска
+     * @param searchType тип поиска.
+     */
+    public void searchFiles(File file, String searchType) {
+        if (!file.isDirectory()) {
+            checkFileName(file, searchType);
+        } else {
+            File[] folderEntries = file.listFiles();
+            for (File entry : folderEntries) {
+                if (!entry.isDirectory()) {
+                    checkFileName(entry, searchType);
+                } else {
+                    searchFiles(entry, searchType);
+                }
+            }
+        }
+    }
+}
