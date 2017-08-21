89d3cd2aacfbd3053daf0518edeb0e3cdcd11ca2
AntonGavr92 <Sith_1@mail.ru>
2017-08-21 16:52:58 +0500

    1. Используя класса Calculator. Сделать класс InteractCalc. [#850]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/InteractCalc.java
@@ -0,0 +1,100 @@
+package ru.job4j.calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Console calculator class.
+ * @author agavrikov
+ * @since 21.08.2017
+ * @version 1
+ */
+public class InteractCalc {
+
+    /**
+     * Object Calculator for calculate.
+     */
+    private Calculator calc;
+
+    /**
+     * Command for get previous result.
+     */
+    private static final String PREV_RESULT = "prev";
+
+    /**
+     * Command for exit from programm.
+     */
+    private static final String EXIT_COMMAND = "exit";
+
+    /**
+     * Constructor for initialization.
+     */
+    public InteractCalc() {
+        this.calc = new Calculator();
+    }
+
+    /**
+     * Main method whith loop, for interaction with console.
+     */
+    public void start() {
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))) {
+            String userInput;
+            while (!(userInput = bf.readLine()).equals(EXIT_COMMAND)) {
+                System.out.println(parserUserInput(userInput));
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+
+    /**
+     * Method for parse user input.
+     * @param userInput user input.
+     * @return result of calculate or message about Incorrect data.
+     */
+    private String parserUserInput(String userInput) {
+        String result;
+        if (userInput.contains(PREV_RESULT)) {
+            userInput = userInput.replace(PREV_RESULT, Double.toString(this.calc.getResult()));
+        }
+        String[] arrUserInput = userInput.split(" ");
+        if (arrUserInput.length < 3) {
+            result = "Incorrect data, try again.";
+        } else {
+            result = calculateNum(Double.parseDouble(arrUserInput[0]), Double.parseDouble(arrUserInput[2]), arrUserInput[1]);
+        }
+        return result;
+    }
+
+    /**
+     * Method for calculate.
+     * @param arg1 first argument
+     * @param arg2 second argument
+     * @param operation operation
+     * @return result of calculate
+     */
+    private String calculateNum(double arg1, double arg2, String operation) {
+        if (operation.equals("+")) {
+            this.calc.add(arg1, arg2);
+        } else if (operation.equals("-")) {
+            this.calc.substruct(arg1, arg2);
+        } else if (operation.equals("/")) {
+            this.calc.div(arg1, arg2);
+        } else if (operation.equals("*")) {
+            this.calc.multiple(arg1, arg2);
+        }
+        return Double.toString(this.calc.getResult());
+    }
+
+    /**
+     * Entry point
+     * @param args params
+     */
+    public static void main(String[] args) {
+        InteractCalc calc = new InteractCalc();
+        calc.start();
+    }
+
+
+}
