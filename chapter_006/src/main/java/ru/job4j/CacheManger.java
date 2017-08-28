03d1b1b3ee5a78b20950b313e8abaf0a02e354d6
AntonGavr92 <Sith_1@mail.ru>
2017-08-28 18:10:41 +0500

    Реализации кеша на SoftReference [#1592]

---
+++ chapter_006/src/main/java/ru/job4j/CacheManger.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+
+/**
+ * FileCacheManager class.
+ * @author agavrikov
+ * @since 28.08.2017
+ * @version 1
+ */
+public abstract class CacheManger<K, V> {
+
+    /**
+     * Cache map.
+     */
+    public final HashMap<K, SoftReference<V>> CacheMap = new HashMap<>();
+
+    /**
+     * Method for adding key/value in map.
+     * @param key key
+     * @param value value
+     */
+    public abstract void add(K key, V value);
+
+    /**
+     * Method for get value by key from map.
+     * @param key key
+     * @return content
+     */
+    public abstract V get(K key);
+
+}
