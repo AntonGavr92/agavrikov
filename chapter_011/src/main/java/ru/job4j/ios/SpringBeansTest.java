507d6d1d5ddfe41f3fb938f6bb10486009a0d79d
AntonGavr92 <Sith_1@mail.ru>
2017-08-31 16:33:43 +0500

    1. Продемонстрировать различные виды создания spring beans. [#2091]

---
+++ chapter_011/src/main/java/ru/job4j/ios/SpringBeansTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.ios;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by gavrikov.a on 31/08/2017.
+ */
+@Component
+public class SpringBeansTest {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        SpringBeansTest sbt = context.getBean(SpringBeansTest.class);
+    }
+}
